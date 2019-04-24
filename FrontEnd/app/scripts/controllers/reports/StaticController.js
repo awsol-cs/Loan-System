78aa832350e01b86f83043c89617d61ef44ad3c2
michael.delacruz7 <michael.delacruz@awsys-i.com>
2019-04-24 10:59:00 +0800

    Downloadable / viewable government documents

--- FrontEnd/app/scripts/controllers/reports/StaticController.js
+++ FrontEnd/app/scripts/controllers/reports/StaticController.js
@@ -1,7 +1,34 @@
 (function (module) {
     mifosX.controllers = _.extend(module, {
         StaticController: function (scope, routeParams, resourceFactory, location, route) {
-            
+           scope.getCurrentDocument = function() {
+           	   scope.isVisible = false;
+
+			   	if(scope.currentDocument.includes(".pdf")){
+			   		scope.isVisible = scope.isVisible = true;
+			   		scope.pdf = {
+			   			src: '../../../static/' + scope.currentDocument,
+			   		};
+			   	}
+			   	else {
+			   	 	var link = document.createElement("a");
+    				link.href = '../../../static/' + scope.currentDocument;
+    				link.style = "visibility:hidden";
+    				//link.download = '../../../static' + scope.currentDocument;
+    				document.body.appendChild(link);
+				    link.click();
+				    document.body.removeChild(link);
+			   	}
+
+		   		
+		   };
+		   /*
+		   scope.pdf = {
+		        src: '../../../static/SEC-Form-LCFS1.pdf',
+		   };*/
+
+
+
         }
     });
     mifosX.ng.application.controller('StaticController', ['$scope', '$routeParams', 'ResourceFactory', '$location', '$route', mifosX.controllers.StaticController]).run(function ($log) {
